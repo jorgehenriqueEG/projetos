19d4cc56dbff2da68d67be030f471888a4215568
jorgehenriqueEG <jorgehenriqueavitorino@edu.uniube.br>
2025-05-22 10:20:42 -0300

    Update Relógio digital simples.py

--- Relógio digital simples.py
+++ Relógio digital simples.py
@@ -6,16 +6,12 @@ def update_time():
     label.config(text=current_time)
     label.after(1000, update_time)
 
-# Configuração da janela principal
 root = tk.Tk()
 root.title("Relógio Digital Simples")
 
-# Configuração do rótulo para exibir o horário
 label = tk.Label(root, font=('Arial', 48), background='black', foreground='white')
 label.pack(anchor='center')
 
-# Inicia a atualização do relógio
 update_time()
 
-# Inicia o loop principal da interface gráfica
 root.mainloop()
